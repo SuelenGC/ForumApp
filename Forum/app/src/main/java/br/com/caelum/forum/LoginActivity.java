db09344355ac79719efed79303782f2b74674d93
suelengc <suelengcarvalho@gmail.com>
2016-07-11 13:58:14 -0300

    Removing unused and generated files from server

---
+++ Forum/app/src/main/java/br/com/caelum/forum/LoginActivity.java
@@ -0,0 +1,14 @@
+package br.com.caelum.forum;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
